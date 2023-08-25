af30ecdd1517dd269bd1deef318fe98c03881086
Vicente Matias <matias.vicente@branded.com.ar>
2023-08-25 11:38:58 -0300

    second commit - entities and relationships

---
+++ src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
@@ -0,0 +1,38 @@
+package com.platzi.pizza.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pizza")
+@Getter
+@Setter
+@NoArgsConstructor //crea un constructor por defecto (sin parametros)
+public class PizzaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pizza", nullable = false)
+    private Integer IdPizza;
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String name;
+
+    @Column(nullable = false,length = 150)
+    private String description;
+
+    //Guarda un maximo de 5 digitos y reserva los ultimos 2 digitos para guardarlos con el decimal (123,45)
+    @Column(nullable = false, columnDefinition = "DECIMAL(5,2)")
+    private Double price;
+
+    @Column(columnDefinition = "TINYINT")
+    private Boolean vegetarian;
+
+    @Column(columnDefinition = "TINYINT")
+    private Boolean vegan;
+
+    @Column(columnDefinition = "TINYINT", nullable = false)
+    private Boolean available;
+}
