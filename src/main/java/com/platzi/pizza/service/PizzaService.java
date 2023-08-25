047e7f3879857546713f3f6376d49f9fe957aff2
Vicente Matias <matias.vicente@branded.com.ar>
2023-08-25 12:23:15 -0300

    Spring data repositories

--- src/main/java/com/platzi/pizza/service/PizzaService.java
+++ src/main/java/com/platzi/pizza/service/PizzaService.java
@@ -1,9 +1,8 @@
 package com.platzi.pizza.service;
 
 import com.platzi.pizza.persistence.entity.PizzaEntity;
+import com.platzi.pizza.persistence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,14 +10,18 @@ import java.util.List;
 @Service
 public class PizzaService {
 
-    private JdbcTemplate jdbcTemplate;
+    private final PizzaRepository pizzaRepository;
 
     @Autowired
-    public PizzaService (JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public PizzaService (PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
     }
 
     public List<PizzaEntity> getAll() {
-        return this.jdbcTemplate.query("SELECT * FROM pizza", new BeanPropertyRowMapper<>(PizzaEntity.class));
+        return this.pizzaRepository.findAll();
+    }
+
+    public PizzaEntity getPizzaById (int idPizza) {
+        return this.pizzaRepository.findById(idPizza).orElse(null);
     }
 }
