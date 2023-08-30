a37f53840574bf9e02ed13df32e4e35d90a025f9
Vicente Matias <matias.vicente@branded.com.ar>
2023-08-30 10:22:21 -0300

    query methods for pizza

--- src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
+++ src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
@@ -3,5 +3,15 @@ package com.platzi.pizza.persistence.repository;
 import com.platzi.pizza.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+
+    List<PizzaEntity> findByAvailableTrueOrderByPrice ();
+
+    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
+
+    List<PizzaEntity> findAllByAvailableTrueAndDescriptionContainingIgnoreCase(String description);
+
+    List<PizzaEntity> findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(String description);
 }
