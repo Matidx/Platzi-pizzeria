1c0dbebda44d712dc3bd14c74b50dfb7e83b7b3d
Vicente Matias <matias.vicente@branded.com.ar>
2023-08-31 15:43:11 -0300

    query methods orders

--- src/main/java/com/platzi/pizza/service/OrderService.java
+++ src/main/java/com/platzi/pizza/service/OrderService.java
@@ -5,12 +5,20 @@ import com.platzi.pizza.persistence.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private static final String DELIVERY = "D";
+    private static final String CARRYOUT = "C";
+
+    private static final String ON_SITE = "S";
+
 
     @Autowired
     public OrderService(OrderRepository orderRepository) {
@@ -19,5 +27,15 @@ public class OrderService {
 
     public List<OrderEntity> getAll() {
         return this.orderRepository.findAll();
+    }
+
+    public List<OrderEntity> getTodayOrders() {
+        LocalDateTime today = LocalDateTime.now();
+        return this.orderRepository.findAllByDateAfter(today);
+    }
+
+    public List<OrderEntity> getOutSideOrders() {
+        List<String> methods = Arrays.asList(DELIVERY, CARRYOUT);
+        return this.orderRepository.findAllByMethodIn(methods);
     }
 }
