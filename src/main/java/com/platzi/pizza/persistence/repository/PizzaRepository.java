9c674706a89457683e08e765c189213c5c021f44
Vicente Matias <matias.vicente@branded.com.ar>
2023-08-31 15:57:24 -0300

    query methods and optional in repository

--- src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
+++ src/main/java/com/platzi/pizza/persistence/repository/PizzaRepository.java
@@ -4,14 +4,17 @@ import com.platzi.pizza.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
 
     List<PizzaEntity> findByAvailableTrueOrderByPrice ();
 
-    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
+    Optional<PizzaEntity> findFirstByAvailableTrueAndNameIgnoreCase(String name);
 
     List<PizzaEntity> findAllByAvailableTrueAndDescriptionContainingIgnoreCase(String description);
 
     List<PizzaEntity> findAllByAvailableTrueAndDescriptionNotContainingIgnoreCase(String description);
+    List<PizzaEntity> findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(double price);
+
 }
